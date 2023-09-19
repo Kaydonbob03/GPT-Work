4c31a1dba1acad28efde1509ed202b5c69c011d8
Kaydonbob03 <142696941+Kaydonbob03@users.noreply.github.com>
2023-09-19 09:50:00 -0400

    fixed response generation using GPT API

--- InputtoResponse.py
+++ InputtoResponse.py
@@ -12,13 +12,16 @@ import openai
 # openai.api_key = api_key
 
 # def generate_response(prompt):
-#     response = openai.Completion.create(
-#         engine="GPT-4", # Set to gpt-3.5-turbo for GPT3.5 or set to gpt-4 for GPT4
-#         prompt=prompt,
-#         temperature=0.2, # Change the temperature from 0 to 1 for more or less creative responses. 0 is more probable, 1 is more creative. Default is 0.2
-#         max_tokens=1500
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",  # Set to gpt-3.5-turbo for GPT3.5 or set to gpt-4 for GPT4
+#         messages=[
+#             {"role": "system", "content": "You are a helpful assistant."},
+#             {"role": "user", "content": prompt}
+#         ],
+#         temperature=0.2,   # Change the temperature from 0 to 1 for more or less creative responses. 0 is more probable, 1 is more creative. Default is 0.2
+#         max_tokens=150     # Specify the maximum number of tokens in the response
 #     )
-#     return response.choices[0].text.strip()
+#     return response.choices[0].message["content"].strip()
 
 
 # while True:
@@ -57,14 +60,18 @@ openai.api_key = api_key
 fixed_prompt = "You are acting as a Canadian fratboy. Please phrase your response as such using 'Bro' and 'eh' frequently.\n"
 
 def generate_response(prompt):
-    full_prompt = fixed_prompt + prompt  # Combine the fixed prompt and user input
-    response = openai.Completion.create(
-        engine="GPT-4", # Set to gpt-3.5-turbo for GPT3.5 or set to gpt-4 for GPT4
-        prompt=full_prompt,
-        temperature=0.2, # Change the temperature from 0 to 1 for more or less creative responses. 0 is more probable, 1 is more creative. Default is 0.2
-        max_tokens=150
+    full_prompt = fixed_prompt + prompt
+    response = openai.ChatCompletion.create(
+        model="gpt-4",  # Set to gpt-3.5-turbo for GPT3.5 or set to gpt-4 for GPT4
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": full_prompt}
+        ],
+        temperature=0.2,   # Change the temperature from 0 to 1 for more or less creative responses. 0 is more probable, 1 is more creative. Default is 0.2
+        max_tokens=150     # Specify the maximum number of tokens in the response
     )
-    return response.choices[0].text.strip()
+    return response.choices[0].message["content"].strip()
+
 
 
 
