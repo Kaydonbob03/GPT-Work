b9349d63aac54a3e78c0e65c91094dd99ed83078
Kaydonbob03 <142696941+Kaydonbob03@users.noreply.github.com>
2023-11-13 15:36:29 -0500

    added error handling

--- InputtoResponse.py
+++ InputtoResponse.py
@@ -11,17 +11,22 @@ api_key = "YOUR_OPENAI_API_KEY"
 # Initialize the OpenAI API client
 openai.api_key = api_key
 
-def generate_response(prompt):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # Set to gpt-3.5-turbo for GPT3.5 or set to gpt-4 for GPT4
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ],
-        temperature=0.2,   # Change the temperature from 0 to 1 for more or less creative responses. 0 is more probable, 1 is more creative. Default is 0.2
-        max_tokens=150     # Specify the maximum number of tokens in the response
-    )
-    return response.choices[0].message["content"].strip()
+def generate_response(prompt, model_name="gpt-4"):
+    try:
+        response = openai.ChatCompletion.create(
+            model=model_name,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        return response['choices'][0]['message']['content'].strip()
+    except openai.error.InvalidRequestError:
+        if model_name != "gpt-3.5-turbo":
+            print("Failed to access GPT-4, falling back to GPT-3.5-turbo.")
+            return generate_response(prompt, "gpt-3.5-turbo")
+        else:
+            raise
 
 
 while True:
@@ -59,18 +64,22 @@ while True:
 # # Fixed prompt that will be added to the beginning of each input
 # fixed_prompt = "You are acting as a Canadian fratboy. Please phrase your response as such using 'Bro' and 'eh' frequently.\n"
 
-# def generate_response(prompt):
-#     full_prompt = fixed_prompt + prompt
-#     response = openai.ChatCompletion.create(
-#         model="gpt-4",  # Set to gpt-3.5-turbo for GPT3.5 or set to gpt-4 for GPT4
-#         messages=[
-#             {"role": "system", "content": "You are a helpful assistant."},
-#             {"role": "user", "content": full_prompt}
-#         ],
-#         temperature=0.2,   # Change the temperature from 0 to 1 for more or less creative responses. 0 is more probable, 1 is more creative. Default is 0.2
-#         max_tokens=150     # Specify the maximum number of tokens in the response
-#     )
-#     return response.choices[0].message["content"].strip()
+# def generate_response(prompt, model_name="gpt-4"):
+#     try:
+#         response = openai.ChatCompletion.create(
+#             model=model_name,
+#             messages=[
+#                 {"role": "system", "content": "You are a helpful assistant."},
+#                 {"role": "user", "content": prompt}
+#             ]
+#         )
+#         return response['choices'][0]['message']['content'].strip()
+#     except openai.error.InvalidRequestError:
+#         if model_name != "gpt-3.5-turbo":
+#             print("Failed to access GPT-4, falling back to GPT-3.5-turbo.")
+#             return generate_response(prompt, "gpt-3.5-turbo")
+#         else:
+#             raise
 
 
 
